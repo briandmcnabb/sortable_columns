5e8c98325860ebc64be35247edab4375d42042c3
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-13 00:15:23 -0400

    Add action_view_extension.

--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -1,3 +1,5 @@
+require 'sortable_columns/helpers/action_view_extension'
+
 module SortableColumns
   ActiveSupport.on_load(:active_record) do
     require 'sortable_columns/active_record_extension'
---
+++ lib/sortable_columns/helper/action_view_extension.rb
@@ -0,0 +1,13 @@
+module SortableColumns
+  module ActionViewExtension
+    extend ::ActiveSupport::Concern
+    module InstanceMethods
+      def sortable(column, title = nil)
+        title ||= column.titleize
+        css_class = column == sort_column ? "current #{sort_direction}" : nil
+        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+        link_to title, params.merge(:sort => column, :direction => direction), {:class => css_class}
+      end
+    end
+  end
+end
