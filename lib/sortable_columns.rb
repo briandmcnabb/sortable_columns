2afff4c0222afaed7ae6f32ec2fdbd97728bb993
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-12 23:46:50 -0400

    Require active_record_extensions.

--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -1,6 +1,6 @@
 module SortableColumns
   ActiveSupport.on_load(:active_record) do
-    require 'kaminari/models/active_record_extension'
+    require 'sortable_columns/active_record_extension'
     ::ActiveRecord::Base.send :include, SortableColumns::ActiveRecordExtension
   end
 end
