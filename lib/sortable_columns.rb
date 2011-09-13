96084da8f35a94572ac7389dd0e07b77a9c735d7
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-13 15:43:58 -0400

    Add ActiveSupport.onload(:action_view) to sortable_columns module.

--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -1,8 +1,11 @@
-require 'sortable_columns/helpers/action_view_extension'
-
 module SortableColumns
   ActiveSupport.on_load(:active_record) do
     require 'sortable_columns/active_record_extension'
     ::ActiveRecord::Base.send :include, SortableColumns::ActiveRecordExtension
+  end
+  
+  ActiveSupport.on_load(:action_view) do
+    require 'sortable_columns/helpers/action_view_extension'
+    ::ActionView::Base.send :include, SortableColumns::ActionViewExtension
   end
 end
