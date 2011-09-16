23dd1044b27e4b1b92ea52e66460fdf540642e82
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-15 22:13:03 -0400

    Update .gitignore.  Clean up tests. Add colors to test results through .rspec file.

--- spec/model/scopes_spec.rb
+++ spec/model/scopes_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 shared_examples_for 'default' do
-  it { should have(50).users }
   its('last.name') { should == 'John A Doe' }
 end
 
@@ -11,27 +10,23 @@ describe SortableColumns::ActiveRecordExtension do
   end
   
   describe '#sortable' do
-    context 'sortable without an argument (defaults)' do
+    context 'sortable without any argument (defaults)' do
       subject { User.sortable }
-      it { should have(50).users }
       it_should_behave_like 'default'
     end
     
     context 'sort with only column argument passed' do
       subject { User.sortable('years_till_retirement') }
-      it { should have(50).users }
       its('first.name') { should == 'John A Doe' }
     end
     
     context 'sort with both arguments passed' do
       subject { User.sortable('years_till_retirement', 'asc') }
-      it { should have(50).users }
       its('last.name') { should == 'John A Doe' }
     end
     
     context 'sort with invalid arguments passed' do
       subject { User.sortable('invalid_column', 'invalid_direction') }
-      it { should have(50).users }
       it_should_behave_like 'default'
     end
   end 
