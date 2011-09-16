10ff0993cf645371a7fbff8d6f22ac70a366f7e6
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-16 17:59:27 -0400

    Update view helper. There was no need for them to call the param validation logic.

--- lib/sortable_columns/helpers/action_view_extension.rb
+++ lib/sortable_columns/helpers/action_view_extension.rb
@@ -3,10 +3,10 @@ module SortableColumns
     extend ActiveSupport::Concern
     
     module InstanceMethods
-      def sortable(column, title = nil)
+      def sortable(column, title = nil)        
         title ||= column.titleize
-        css_class = column == sort_column ? "current #{sort_direction}" : nil
-        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+        css_class = column == params[:sort] ? "current #{ params[:direction] }" : nil
+        direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
         link_to title, params.merge(:sort => column, :direction => direction), {:class => css_class}
       end
     end    
