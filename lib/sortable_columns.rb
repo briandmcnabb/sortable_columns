474ae6e8247cf28b95cb7cc990af32997db635cf
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-17 17:27:29 -0400

    Move initialization to railtie.rb.

--- lib/sortable_columns.rb
+++ lib/sortable_columns.rb
@@ -1,15 +1,17 @@
-require 'rails'
-require 'sortable_columns/config'
-require 'sortable_columns/configuration_methods'
+require 'sortable_columns/railtie'
 
-module SortableColumns  
-  ActiveSupport.on_load(:active_record) do
-    require 'sortable_columns/active_record_extension'
-    include SortableColumns::ActiveRecordExtension
-  end
-  
-  ActiveSupport.on_load(:action_view) do
-    require 'sortable_columns/helpers/action_view_extension'
-    include SortableColumns::ActionViewExtension
-  end
-end
+#require 'rails'
+#require 'sortable_columns/config'
+#require 'sortable_columns/configuration_methods'
+#
+#module SortableColumns  
+#  ActiveSupport.on_load(:active_record) do
+#    require 'sortable_columns/active_record_extension'
+#    include SortableColumns::ActiveRecordExtension
+#  end
+#  
+#  ActiveSupport.on_load(:action_view) do
+#    require 'sortable_columns/helpers/action_view_extension'
+#    include SortableColumns::ActionViewExtension
+#  end
+#end
---
+++ lib/sortable_columns/railtie.rb
@@ -0,0 +1,19 @@
+require 'rails'
+require 'sortable_columns/config'
+require 'sortable_columns/configuration_methods'
+
+module SortableColumns
+  class Railtie < ::Rails::Railtie #:nodoc:
+    initializer 'sortable_columns' do |app|
+      ActiveSupport.on_load(:active_record) do
+        require 'sortable_columns/active_record_extension'
+        include SortableColumns::ActiveRecordExtension
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        require 'sortable_columns/helpers/action_view_extension'
+        include SortableColumns::ActionViewExtension
+      end
+    end
+  end
+end
