1a33949c402ccf67f37ffa82bcd7579fa8fb9fac
Brian McNabb <brian.d.mcnabb@gmail.com>
2011-09-17 15:50:36 -0400

    Changed view helper.

--- lib/sortable_columns/helpers/action_view_extension.rb
+++ lib/sortable_columns/helpers/action_view_extension.rb
@@ -5,8 +5,8 @@ module SortableColumns
     module InstanceMethods
       def sortable(column, title = nil)        
         title ||= column.titleize
-        css_class = column == params[:sort][:column] ? "current #{ params[:sort][:direction] }" : nil
-        direction = column == params[:sort][:column] && params[:sort][:direction] == "asc" ? "desc" : "asc"
+        css_class = column == params[:column] ? "current #{ params[:direction] }" : nil
+        direction = column == params[:column] && params[:direction] == "asc" ? "desc" : "asc"
         link_to title, params.merge(column: column, direction: direction), {:class => css_class}
       end
     end    
