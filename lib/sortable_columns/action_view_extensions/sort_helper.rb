1884fb469775afcbe1a40120697c024f0f714032
Brian McNabb <brian.d.mcnabb@gmail.com>
2012-02-14 02:15:13 -0500

    Update sort_helper. Bump version.

--- lib/sortable_columns/action_view_extensions/sort_helper.rb
+++ lib/sortable_columns/action_view_extensions/sort_helper.rb
@@ -2,11 +2,12 @@ module SortableColumns
   module ActionViewExtensions
     module SortHelper
       
-      def sortable(column, title = nil)        
-        title ||= column.titleize
+      def sortable(column, options={})        
+        title  = options[:title]  || column.titleize
+        remote = options[:remote] || false
         css_class = column == params[:column] ? "current #{ params[:direction] }" : nil
         direction = column == params[:column] && params[:direction] == "asc" ? "desc" : "asc"
-        link_to title, params.merge(column: column, direction: direction), {:class => css_class}
+        link_to title, params.merge(column: column, direction: direction, page: nil), { remote: remote, class: css_class }
       end
     end
   end
